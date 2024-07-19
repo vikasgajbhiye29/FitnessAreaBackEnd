b136076bfb24cbf12c2bcd42322cec1540084973
vikasgajbhiye <gajbhiye.vikas29@gmail.com>
2024-07-19 14:53:44 +0530

    Start BackEnd Project for GymManagementSystem

---
+++ src/main/java/com/fitnessarea/FitnessAreaBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.fitnessarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAreaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAreaBackEndApplication.class, args);
+		System.out.println("Program Started....!");
+	}
+
+}
